c85c09fbcb17e0e8dd210d7b6603fa493cbae8b2
knoxiboy <karankmt.tripathi@gmail.com>
2026-03-14 15:54:40 +0530

    Fixed all errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, DM_Sans } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import {
   ClerkProvider,
 } from '@clerk/nextjs'
 import { Provider } from "./provider";
 
-
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 
 const AppFont = DM_Sans({
